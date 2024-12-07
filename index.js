8776fba24517e381566111b2eabe804371487f18
RaresIgescu <raresigescu@gmail.com>
2024-12-07 13:43:01 +0200

    First request

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import axios from "axios";
+
+const app = express();
+const port = 3000;
+const apiKey = "9748aad4-09a4-4572-86ec-fb157d8139fe";
+
+app.get("/", async (req, res) => {
+    res.render("index.ejs");
+});
+
+app.get("/get-countries", async (req, res) => {
+    const result = await axios.get("http://api.airvisual.com/v2/countries?key=" +  apiKey);
+    console.log(result.data);
+    res.redirect("/");
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
