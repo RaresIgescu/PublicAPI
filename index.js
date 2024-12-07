3eb16fc10bbd9714c923687081c7ebc9d69e7ab6
RaresIgescu <raresigescu@gmail.com>
2024-12-07 14:35:09 +0200

    Add all chosen endpoints

--- index.js
+++ index.js
@@ -15,6 +15,30 @@ app.get("/get-countries", async (req, res) => {
     res.redirect("/");
 });
 
+app.get("/get-city-ranking", async (req, res) => {
+    const result = await axios.get("http://api.airvisual.com/v2/city?city=Los%20Angeles&state=California&country=USA&key=" + apiKey);
+    console.log(result.data);
+    res.redirect("/");
+});
+
+app.get("/get-supported-states", async(req, res) => {
+    const result = await axios.get("http://api.airvisual.com/v2/states?country=Romania&key=" + apiKey);
+    console.log(result.data);
+    res.redirect("/");
+});
+
+app.get("/get-supported-cities", async(req, res) => {
+    const result = await axios.get("http://api.airvisual.com/v2/cities?state=Alabama&country=USA&key=" + apiKey);
+    console.log(result.data);
+    res.redirect("/");
+});
+
+app.get("/get-city", async(req, res) => {
+    const result = await axios.get("http://api.airvisual.com/v2/city?city=Vaslui&state=Vaslui&country=Romania&key=" + apiKey)
+    console.log(result.data);
+    res.redirect("/");
+});
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
